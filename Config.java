37aad9983ae17d39a9e242b5ca4ce590d4577dd0
Alumno2221069 <94260485+Alumno2221069@users.noreply.github.com>
2021-11-14 03:14:17 -0600

    Add files via upload

---
+++ Config.java
@@ -0,0 +1,11 @@
+package com.MiEmpresa.Programa;
+
+public class Config {
+
+    private Config(){}
+
+    static String credenciales_ruta = System.getProperty( "user.dir" )+"/src/files/Credenciales.txt";
+    static String productos_ruta = System.getProperty( "user.dir" )+"/src/files/Productos.txt";
+
+
+}
